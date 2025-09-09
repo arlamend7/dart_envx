18a701ee50afb54bbda61032d7f4bc4f8a4f45ce
Arlan mendes <arlanmendes197@gmail.com>
2025-09-09 17:52:48 -0400

    update example code

--- example/envx_example.dart
+++ example/envx_example.dart
@@ -1,8 +1,32 @@
-import 'environment/environment.dart';
+import 'package:envx/envx.dart';
+
+import './environment/enum/app_environment.dart';
+import './environment/models/example_config.dart';
+
+import './environment/configs/environment.development.dart' as development;
+import './environment/configs/environment.production.dart' as production;
+
+ExampleConfig? environment;
+
+var _register = Environment.register(
+  configuration: {
+    AppEnvironment.development: development.environment,
+    AppEnvironment.production: production.environment,
+  },
+  resolver: aliasesResolver({
+    AppEnvironment.development: ['development', 'dev'],
+    AppEnvironment.production: ['production', 'prod'],
+  }),
+  defaultEnvironment: AppEnvironment.development,
+  defineKey: 'APP_ENV',
+);
+
+Future environmentInitialize() async {
+  environment = await _register.current();
+}
 
 Future<void> main() async {
   await environmentInitialize();
 
-  final config = environment;
-  print('Base URL: ${config?.baseUrl}');
+  print('Base URL: ${environment?.baseUrl}');
 }
